43c78169505270465d591e6c6927d8fd0c46967e
badalkhatiwara <badalkhatiwara@bhutanndi.bt>
2026-08-20 21:44:51 +0600

    Import CSS from Payload CMS in layout component

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -13,8 +13,8 @@ import type { ServerFunctionClient } from "payload";
 import config from "@payload-config";
 import { RootLayout, handleServerFunctions } from "@payloadcms/next/layouts";
 import React from "react";
-
 import { importMap } from "./admin/importMap";
+import "@payloadcms/next/css";
 import "./custom.scss";
 
 type Args = {
